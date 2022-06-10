c19fbbc8eff2f53f28aed178acca723741896fc9
lucaeyring <luca.eyring@googlemail.com>
2022-06-10 11:07:07 +0200

    changed gaussian dataset

--- neural_ot/utils/__init__.py
+++ neural_ot/utils/__init__.py
@@ -1,3 +1,3 @@
-from .data_utils import get_anndata_loaders
+from .data_utils import get_anndata_loaders, get_gaussian_mixture_loaders
 from .neural_dual import NeuralDual, NeuralDualSolver
 from .ot_utils import sinkhorn_loss
--- neural_ot/utils/data_utils.py
+++ neural_ot/utils/data_utils.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 
 
 def get_anndata_loaders(
-    adata_path: str, use_pca: bool = False, batch_size: int = 32, rng: np.random.Generator = np.random.default_rng()
+    adata_path: str, use_pca: bool, rng: np.random.Generator, batch_size: int = 64
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """Load AnnData object, split data and return a tuple of data loaders."""
     adata = sc.read(adata_path, backed="r+", cache=True)
@@ -27,9 +27,39 @@ def get_anndata_loaders(
         test_data = adata.X[test_indices]
     # create datasets & loaders
     train_dataset = NumpyDataset(train_data)
-    val_dataset = NumpyDataset(val_data)
+    valid_dataset = NumpyDataset(val_data)
     test_dataset = NumpyDataset(test_data)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
-    return train_loader, val_loader, test_loader
+    return train_loader, valid_loader, test_loader
+
+
+def get_gaussian_mixture_loaders(
+    centers: np.ndarray,
+    sigma: np.ndarray,
+    input_dim: int,
+    rng: np.random.Generator,
+    num_samples: int = 15000,
+    batch_size: int = 64,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """Generate Gaussian mixture data, split data and return a tuple of data loaders."""
+    # generate data
+    cholesky_l = np.linalg.cholesky(sigma + 1e-4 * np.eye(input_dim))
+    u = rng.normal(loc=0, scale=1, size=[num_samples, input_dim])
+    center = centers[rng.choice(len(centers), size=num_samples), :]
+    data = center + np.tensordot(u, cholesky_l, axes=1)
+    # split data
+    indices = np.arange(len(data))
+    rng.shuffle(indices)
+    train_data = data[indices[: int(np.round(len(data) * 0.8))]]
+    val_data = data[indices[int(np.round(len(data) * 0.8)) : int(np.round(len(data) * 0.9))]]
+    test_data = data[indices[int(np.round(len(data) * 0.9)) :]]
+    # create datasets & loaders
+    train_dataset = NumpyDataset(train_data)
+    valid_dataset = NumpyDataset(val_data)
+    test_dataset = NumpyDataset(test_data)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    return train_loader, valid_loader, test_loader
