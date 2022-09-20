776106c285d4fd852cbc9a77cb0349329e1ea88c
lucaeyring <luca.eyring@googlemail.com>
2022-09-20 11:00:57 +0200

    added matching pair sampler

--- neural_ot/data/jax_data.py
+++ neural_ot/data/jax_data.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple
 
 import jax
 import jax.numpy as jnp
+import ott
 
 
 @dataclass
@@ -33,6 +34,59 @@ class JaxSampler:
         """Sample data."""
         if full_dataset:
             return self.data
+        else:
+            return self._sample(key)
+
+
+@dataclass
+class MatchingPairSampler:
+    """Data sampler for Jax."""
+
+    data_source: jnp.ndarray
+    data_target: jnp.ndarray
+    batch_size: int = 128
+    tau_a: float = 1.0
+    tau_b: float = 1.0
+    epsilon: float = 1.0
+    weighting: Optional[jnp.ndarray] = None
+
+    def __post_init__(self):
+        self.length_source = len(self.data_source)
+        self.length_target = len(self.data_target)
+        self.ott_scaling = jnp.ones(self.batch_size) / self.batch_size
+
+        @jax.jit
+        def _sample(key: jax.random.KeyArray) -> Tuple[jnp.ndarray, jnp.ndarray]:
+            """Jitted sample function."""
+            key_source, key_target, key_indices = jax.random.split(key, 3)
+            if self.weighting is None:
+                batch_source = self.data_source[
+                    jax.random.randint(key_source, shape=[self.batch_size], minval=0, maxval=self.length_source)
+                ]
+            else:
+                batch_source = jax.random.choice(key, self.data_source, shape=[self.batch_size], p=self.weighting)
+            batch_target = self.data_target[
+                jax.random.randint(key_target, shape=[self.batch_size], minval=0, maxval=self.length_target)
+            ]
+            # solve regularized ot between batch_source and batch_target
+            geom = ott.geometry.pointcloud.PointCloud(batch_source, batch_target, epsilon=self.epsilon)
+            out = ott.core.sinkhorn.sinkhorn(
+                geom, self.ott_scaling, self.ott_scaling, tau_a=self.tau_a, tau_b=self.tau_b, jit=False
+            )
+            # get flattened log transition matrix
+            transition_matrix = jnp.log(geom.transport_from_potentials(out.f, out.g).flatten())
+            # sample from transition_matrix
+            indeces = jax.random.categorical(key_indices, transition_matrix, shape=[self.batch_size])
+            indeces_source = indeces // self.batch_size
+            indeces_target = indeces % self.batch_size
+            return batch_source[indeces_source], batch_target[indeces_target]
+
+        self._sample = _sample
+
+    def __call__(self, key: jax.random.KeyArray, full_dataset: bool = False) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        """Sample data."""
+        if full_dataset:
+            return self.data_source, self.data_target
         else:
             return self._sample(key)
 
